7b8b6327dfacc409c218ab64dd5613bd87bb078a
Romain BUREL <generic3@diademe.fr>
2024-12-30 09:44:07 +0100

    refactor: split RewardsService.CalculateRewards + add & use CalculateLocationRewards private function + improve readability & maintanability

--- Api/Services/RewardsService.cs
+++ Api/Services/RewardsService.cs
@@ -37,7 +37,6 @@ public class RewardsService : IRewardsService
         ConcurrentBag<VisitedLocation> userLocations = user.VisitedLocations;
         List<Attraction> attractions = _gpsUtil.GetAttractions();
         var newUserRewards = new ConcurrentBag<UserReward>();
-        var visitedAttractionsName = new List<string>();
 
         var visitedAttractions = attractions.Where(a => userLocations.Any(ul => NearAttraction(ul, a)));
         var relevantLocations = userLocations.Where(ul => visitedAttractions.Any(a => NearAttraction(ul, a)));
@@ -45,21 +44,7 @@ public class RewardsService : IRewardsService
         var visitedLocationTasks = new List<Task>();
         foreach (var visitedLocation in relevantLocations)
         {
-            visitedLocationTasks.Add(Task.Run(async () =>
-            {
-                var nearNotVisitedAttractions = visitedAttractions.Where(a => !visitedAttractionsName.Contains(a.AttractionName) && NearAttraction(visitedLocation, a));
-                var attractionTasks = new List<Task>();
-                foreach (var attraction in nearNotVisitedAttractions)
-                {
-                    visitedAttractionsName.Add(attraction.AttractionName);
-                    attractionTasks.Add(Task.Run(() =>
-                    {
-                        newUserRewards.Add(new UserReward(visitedLocation, attraction, GetRewardPoints(attraction, user)));
-                    }));
-                }
-
-                await Task.WhenAll(attractionTasks);
-            }));
+            visitedLocationTasks.Add(CalculateLocationRewards(user, newUserRewards, visitedAttractions, visitedLocation));
         }
 
         await Task.WhenAll(visitedLocationTasks);
@@ -76,6 +61,39 @@ public class RewardsService : IRewardsService
         return GetDistance(attraction, location) <= _attractionProximityRange;
     }
 
+    public double GetDistance(Locations loc1, Locations loc2)
+    {
+        double lat1 = Math.PI * loc1.Latitude / 180.0;
+        double lon1 = Math.PI * loc1.Longitude / 180.0;
+        double lat2 = Math.PI * loc2.Latitude / 180.0;
+        double lon2 = Math.PI * loc2.Longitude / 180.0;
+
+        double angle = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(lon1 - lon2));
+
+        double nauticalMiles = 60.0 * angle * 180.0 / Math.PI;
+        return StatuteMilesPerNauticalMile * nauticalMiles;
+    }
+
+    private Task CalculateLocationRewards(User user, ConcurrentBag<UserReward> newUserRewards, IEnumerable<Attraction> visitedAttractions, VisitedLocation visitedLocation)
+    {
+        var visitedAttractionsName = new List<string>();
+        return Task.Run(async () =>
+        {
+            var nearNotVisitedAttractions = visitedAttractions.Where(a => !visitedAttractionsName.Contains(a.AttractionName) && NearAttraction(visitedLocation, a));
+            var attractionTasks = new List<Task>();
+            foreach (var attraction in nearNotVisitedAttractions)
+            {
+                visitedAttractionsName.Add(attraction.AttractionName);
+                attractionTasks.Add(Task.Run(() =>
+                {
+                    newUserRewards.Add(new UserReward(visitedLocation, attraction, GetRewardPoints(attraction, user)));
+                }));
+            }
+
+            await Task.WhenAll(attractionTasks);
+        });
+    }
+
     private bool NearAttraction(VisitedLocation visitedLocation, Attraction attraction)
     {
         return GetDistance(attraction, visitedLocation.Location) <= _proximityBuffer;
@@ -84,19 +102,5 @@ public class RewardsService : IRewardsService
     private int GetRewardPoints(Attraction attraction, User user)
     {
         return _rewardsCentral.GetAttractionRewardPoints(attraction.AttractionId, user.UserId);
-    }
-
-    public double GetDistance(Locations loc1, Locations loc2)
-    {
-        double lat1 = Math.PI * loc1.Latitude / 180.0;
-        double lon1 = Math.PI * loc1.Longitude / 180.0;
-        double lat2 = Math.PI * loc2.Latitude / 180.0;
-        double lon2 = Math.PI * loc2.Longitude / 180.0;
-
-        double angle = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2)
-                                + Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(lon1 - lon2));
-
-        double nauticalMiles = 60.0 * angle * 180.0 / Math.PI;
-        return StatuteMilesPerNauticalMile * nauticalMiles;
     }
 }
